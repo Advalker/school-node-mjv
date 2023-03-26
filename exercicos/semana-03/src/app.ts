44ddaa4cac26a460b667f71d87948b9173d0f2e7
Advalker <80931600+Advalker@users.noreply.github.com>
2023-03-26 13:18:55 -0300

    terceiro commit

--- exercicos/semana-03/src/app.ts
+++ exercicos/semana-03/src/app.ts
@@ -1,22 +1,15 @@
 import express from 'express';
-import { Request, Response, Router } from 'express';
 import cors from 'cors';
+import router from './routers';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-const router = Router();
-
-router.get('/', (req: Request, res: Response) => {
-    const helloWord = { message: 'Aplicação funcionando com sucesso! ' };
-    res.send(helloWord);
-});
-
 app.use(router);
 
 const port = 3000;
+
 app.listen(port, () => {
     console.log('Aplicação online na porta: ', port);
 })
